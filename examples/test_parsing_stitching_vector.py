96989810038df9a50b9c2c5c41ee9e153eba3efe
agerardin <antoine.gerardin@gmail.com>
2023-09-01 00:09:01 -0400

    finalize algorithm

--- examples/test_parsing_stitching_vector.py
+++ examples/test_parsing_stitching_vector.py
@@ -1,6 +1,6 @@
 import filepattern as fp
 import pprint
-from bfio import BioReader
+from bfio import BioReader, BioWriter
 import numpy as np
 from math import ceil
 from pathlib import Path
@@ -12,6 +12,8 @@ def main():
     # IMG_PATH = Path("/Users/antoinegerardin/Documents/projects/polus-plugins/transforms/images/image-assembler-plugin/data/nist_mist_dataset/Small_Phase_Test_Dataset/image-tiles")
     IMG_PATH = Path("/Users/antoinegerardin/Documents/data/image-assembler-plugin/nist-mist-dataset/images")
     pattern = fp.infer_pattern(STITCH_PATH)
+
+    OUTPUT_PATH = Path("examples/data") / "out.ome.tiff"
 
     fovs = fp.FilePattern(STITCH_PATH, pattern)
 
@@ -42,38 +44,47 @@ def main():
     print(size)
     print(fov_width)
 
-    # find final image size and bin each image into one
-    # or several chunk definition.
-    full_image_width, full_image_height = fov_width, fov_height
+    with BioReader(first_image) as br:
+     full_image_metadata = br.metadata
+     size = br.image_size
+     fov_width = br.x
+     fov_height = br.y
+     # TODO CHECK if there is a need to assemble 3D fovs.
+     # It is unlikely given that some stitching 
+     # and alignment would needed to occur beforehand.
 
+    # find final image size
+    full_image_width, full_image_height = fov_width, fov_height
     for fov in fovs():
         metadata = fov[0]
         full_image_width = max(full_image_width, metadata['posX'] + fov_width)
         full_image_height = max(full_image_height, metadata['posY'] + fov_height)
+
         
     print("full image size: ", full_image_width, full_image_height)
 
-    # we need to region of fov to one or more chunks
+    # number of chunks we need to write in the x,y plane
     chunk_grid_col = ceil(full_image_width / chunk_width)
     chunk_grid_row = ceil(full_image_height / chunk_height)
     chunk_grid_size = (chunk_grid_col, chunk_grid_row)
 
-    
-    chunks = [[]] * chunk_grid_row * chunk_grid_col
-
-    print(chunks)
+    chunks = [[[] for col in range(chunk_grid_col)] for row in range(chunk_grid_row)]
 
     region_count = 0
 
+    # figure out regions of fovs that needs to be copied into each chunk.
+    # This is fast so it can be done beforehand in a single process.
     for fov in fovs():
-        # get global coordinates of fov
-        metadata = fov[0]
+        # we are parsing a stitching vector, so we are always getting unique records.        
         assert(len(fov[1]) == 1)
         filename = fov[1][0]
+
+        # get global coordinates of fov
+        metadata = fov[0]
         global_fov_start_x = metadata['posX']
         global_fov_start_y = metadata['posY']
 
-        # check which chunks this fov overlaps
+        # check which chunks the fov overlaps
         chunk_col_min = global_fov_start_x // chunk_width
         chunk_col_max = (global_fov_start_x + fov_width) // chunk_width
         chunk_row_min = global_fov_start_y // chunk_height
@@ -82,7 +93,7 @@ def main():
         max_end_x = 0
         max_end_y = 0
 
-        # define fov's contributions to each chunk
+        # define fov's contributions to each chunk as regions
         for row in range(chunk_row_min, chunk_row_max + 1):
             for col in range(chunk_col_min, chunk_col_max + 1):
                 print("row, col : ", row, col)
@@ -91,6 +102,7 @@ def main():
                 global_end_x = min(global_fov_start_x + fov_width, (col + 1) * chunk_width)
                 global_start_y = max( global_fov_start_y, row * chunk_height)
                 global_end_y = min(global_fov_start_y + fov_height, (row + 1) * chunk_height)
+
                 print(f"{global_start_x}->{global_end_x},{global_start_y}->{global_end_y}") # remove
 
                 assert(global_start_x >= col * chunk_width)
@@ -99,22 +111,22 @@ def main():
                 assert(global_start_y >= row * chunk_height)
                 assert(global_start_y <= (row + 1) * chunk_height)
 
-                # where to start copying in the fov
-                global_fov_start_x = max(global_fov_start_x, col * chunk_width)
-                global_fov_start_y = max(global_start_y, row * chunk_height)
+                # local coordinates within the fov itself
+                fov_start_x = max(global_fov_start_x, col * chunk_width) - global_fov_start_x
+                fov_start_y = max(global_start_y, row * chunk_height) - global_fov_start_y
 
                 region = (
                         filename, 
                         (global_start_x, global_end_x, global_start_y, global_end_y), 
-                        (global_fov_start_x, global_fov_start_y)
+                        (fov_start_x, fov_start_y)
                         )
                 
                 print(f"region : {region}")
 
-                chunks[row * col].append(region)
+                chunks[row][col].append(region)
 
                 # TODO remove
-                region_count = region_count + 1 # TODO remove
+                region_count = region_count + 1
                 max_end_x = max (max_end_x, global_end_x)
                 max_end_y = max (max_end_y, global_end_y)
 
@@ -124,10 +136,16 @@ def main():
 
     print("# of regions: ", region_count)
 
+    # now copy each fov regions.
+    # This requires multiple reads and copies and a final write.
+    # This is a slow IObound process so it can benefit multithreading
+    # TODO add multithreading.
     for row in range(chunk_grid_row):
         for col in range(chunk_grid_col):
 
-            for region in chunks[row * col]:
+            chunk = np.zeros((chunk_width, chunk_height)) # remove,we use bfio supertile
+
+            for region in chunks[row][col]:
 
                 print(f"process region : {region} for chunk ({col},{row})")
                 # TODO check if we can have subdirectories,
@@ -136,9 +154,6 @@ def main():
                 with BioReader(filepath) as br:
                     (global_start_x, global_end_x, global_start_y, global_end_y) = region[1]
                     (global_fov_start_x, global_fov_start_y) = region[2]
-                    
-                    # chunk_start_x = global_start_x - col * chunk_width
-                    # chunk_start_y = global_start_y - row * chunk_height
 
                     region_width = global_end_x - global_start_x
                     region_height = global_end_y - global_start_y
@@ -147,7 +162,13 @@ def main():
 
                     br[global_fov_start_y: global_fov_start_y + region_height ,global_fov_start_x:global_fov_start_x + region_width]
 
-    
+                    chunk_start_x = global_start_x - col * chunk_width
+                    chunk_start_y = global_start_y - row * chunk_height
+                    chunk_end_x = chunk_start_x + region_width
+                    chunk_end_y = chunk_start_y + region_height
+
+                    print(f"to chunk {chunk_start_x}->{chunk_end_x} , {chunk_start_y}->{chunk_end_y}")
+            
 
 if __name__ == "__main__":
     main()
